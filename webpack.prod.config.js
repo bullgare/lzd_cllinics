e328a99002feeace4db27bae12673bfba5779740
bullgare <bullgare@gmail.com>
2016-02-13 13:22:22 +0300

    Deploy to another dir

---
+++ webpack.prod.config.js
@@ -0,0 +1,69 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var config = {
+    entry: path.resolve(__dirname, 'src/index.jsx'),
+    output: {
+        path: path.resolve(__dirname, 'deploy'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loaders: ['react-hot', 'babel'],
+            },
+            {
+                test: /\.js?$/,
+                exclude: /(node_modules|bower_components)/,
+                loaders: ['babel'],
+            },
+
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "file"
+            },
+            {
+                test: /\.(woff|woff2)$/,
+                loader: "url?prefix=font/&limit=5000"
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/octet-stream"
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=image/svg+xml"
+            },
+            {
+                test: /\.gif/,
+                loader: "url-loader?limit=10000&mimetype=image/gif"
+            },
+            {
+                test: /\.jpg/,
+                loader: "url-loader?limit=10000&mimetype=image/jpg"
+            },
+            {
+                test: /\.png/,
+                loader: "url-loader?limit=10000&mimetype=image/png"
+            }
+
+        ]
+    },
+    devServer: {
+        contentBase: "./public",
+        noInfo: true, //  --no-info option
+        hot: true,
+        inline: true
+    }
+};
+
+module.exports = config;
