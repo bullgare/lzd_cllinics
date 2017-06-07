4f9abc4066113ab9bab17d65cce1a2a04c9023a8
bullgare <bullgare@gmail.com>
2017-06-07 21:13:56 +0300

    2017 clinics - chains detector

--- utils/parser/main.go
+++ utils/parser/main.go
@@ -8,8 +8,10 @@ import (
 	"log"
 	"net/http"
 	"regexp"
+	"sort"
 	"strconv"
 	"strings"
+	"unicode/utf8"
 )
 
 var (
@@ -51,18 +53,20 @@ func main() {
 	clinicsDescs := linesToClinicsDesc(lines)
 	errs := make([]error, 0)
 
-	var clinics []Clinics
+	var clinics []*Clinics
 	l := len(clinicsDescs)
 	for i, cd := range clinicsDescs {
 		c, err := parse(cd, i)
 		if len(c.Address) > 0 {
-			clinics = append(clinics, c)
+			clinics = append(clinics, &c)
 		}
 		if err != nil {
 			errs = append(errs, err)
 		}
 		log.Printf("%d/%d\n", i+1, l)
 	}
+
+	DetectChains(clinics)
 
 	js, err := json.Marshal(clinics)
 	if err != nil {
@@ -303,4 +307,200 @@ func GetCoordsForAddress(addr string) ([]float64, error) {
 	//// @debug
 	//fmt.Printf("\n\n------bububu: %s -> %s; %s\n\n", yaURL + addr, body, c.coords)
 	return c.coords, nil
+}
+
+//=======================================detecting chains
+func DetectChains(list []*Clinics) {
+	groupedByName := map[string][]int{}
+	for i, c := range list {
+		variants := getChainVariants(c.Name)
+		for _, v := range variants {
+			existing, ok := groupedByName[v]
+			if !ok {
+				existing = []int{}
+			}
+			existing = append(existing, i)
+			groupedByName[v] = existing
+		}
+	}
+
+	chainsWithIds := map[string][]int{}
+	for name, ids := range groupedByName {
+		if len(ids) < 3 {
+			continue
+		}
+		chainsWithIds[name] = ids
+	}
+
+	//for name, ids := range chainsWithIds {
+	//	log.Printf("%s: %v", name, ids)
+	//}
+	//log.Println("------------")
+	chainsWithIds = stripDuplicates(chainsWithIds)
+	//for name, ids := range chainsWithIds {
+	//	log.Printf("%s: %v", name, ids)
+	//}
+
+	for name, ids := range chainsWithIds {
+		for _, id := range ids {
+			if list[id].Chain != "" {
+				log.Fatalf("Tried to set chain %s to clinic that alresdy has a chain: %v", name, list[id])
+			}
+
+			list[id].Chain = name
+		}
+	}
+}
+
+// generates all possible variants of chain name by clinic name
+func getChainVariants(name string) []string {
+	name = strings.Replace(name, "(", "", -1)
+	name = strings.Replace(name, ")", "", -1)
+	name = strings.Replace(name, ",", "", -1)
+	all := strings.Split(name, " ")
+
+	variants := []string{}
+	variantsByStep := make([][]string, len(all))
+	for i, token := range all {
+		if len(token) == 0 {
+			continue
+		}
+		prev := []string{}
+		cur := []string{}
+		if i > 0 {
+			prev = variantsByStep[i-1]
+		}
+		for j := range prev {
+			cur = append(cur, prev[j]+" "+token)
+		}
+		cur = append(cur, token)
+		variantsByStep[i] = cur
+	}
+
+	for _, step := range variantsByStep {
+		for _, v := range step {
+			variants = append(variants, v)
+		}
+	}
+
+	final := []string{}
+	for _, v := range variants {
+		low := strings.ToLower(v)
+		if low == "клиника" ||
+			low == "клиники" ||
+			low == "клиника в" ||
+			low == "клиника на" ||
+			low == "автозаводской\"" ||
+			low == "россии" {
+			continue
+		}
+		if strings.HasPrefix(low, "на ") {
+			continue
+		}
+		if strings.HasSuffix(low, " на") {
+			continue
+		}
+		if utf8.RuneCountInString(v) <= 7 {
+			continue
+		}
+		final = append(final, v)
+	}
+	//if name == "АО \"Группа компаний \"Медси\" на Дубининской" {
+	//	log.Printf("%#v", variants)
+	//	panic("!")
+	//}
+	return final
+}
+
+// here we find the longest suffix string with maximum number of clinics
+// we know that one clinic is only in one chain
+func stripDuplicates(chainsWithIds map[string][]int) map[string][]int {
+	names := []string{}
+	lens := []int{}
+
+	for name := range chainsWithIds {
+		names = append(names, name)
+	}
+
+	sort.Slice(names, func(i, j int) bool {
+		return len(names[i]) < len(names[j])
+	})
+
+	for _, name := range names {
+		lens = append(lens, len(chainsWithIds[name]))
+	}
+
+	// filtering simple duplicates for names that are wrappers for other names
+	// we find the max name that has these clinics
+	// (we assume that one clinic is in one chain only)
+	duplicates := map[int]bool{}
+	filtered := map[string][]int{}
+	for i1, n1 := range names {
+		nameThatFits := n1
+		indexThatFits := i1
+		if duplicates[i1] {
+			continue
+		}
+		for i2 := i1 + 1; i2 < len(names); i2++ {
+			n2 := names[i2]
+			// if we have longer name that fits, use it
+			if strings.Contains(n2, nameThatFits) {
+				if lens[i2] >= lens[indexThatFits] {
+					nameThatFits = n2
+					indexThatFits = i2
+				} else {
+					duplicates[i2] = true
+				}
+			} else if strings.Contains(nameThatFits, n2) {
+				duplicates[i2] = true
+			}
+		}
+		filtered[nameThatFits] = chainsWithIds[nameThatFits]
+	}
+
+	// we know that one clinic can be in several chains - filtering by maximum clinics number
+	clinicChains := map[int]map[string]int{}
+	for name, ids := range chainsWithIds {
+		for _, id := range ids {
+			if _, ok := clinicChains[id]; !ok {
+				clinicChains[id] = map[string]int{}
+			}
+			clinicChains[id][name] = len(filtered[name])
+		}
+	}
+
+	duplicateNames := map[string]bool{}
+
+	for _, chains := range clinicChains {
+		if len(chains) < 2 {
+			continue
+		}
+
+		maxClinics := 0
+		biggestChainName := ""
+		for name, quantity := range chains {
+			if quantity > maxClinics {
+				maxClinics = quantity
+				biggestChainName = name
+			}
+		}
+		if biggestChainName == "" {
+			continue
+		}
+
+		for name := range chains {
+			if name != biggestChainName {
+				duplicateNames[name] = true
+			}
+		}
+	}
+
+	unique := map[string][]int{}
+	for name, ids := range filtered {
+		if !duplicateNames[name] {
+			unique[name] = ids
+		}
+	}
+
+	return unique
 }
